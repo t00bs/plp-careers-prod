08dc8684a4dbb72550993e9d1f3e82ebf4b41503
t00bs <148378245+t00bs@users.noreply.github.com>
2024-06-27 10:44:44 +0100

    Update select-input.js

--- select-input.js
+++ select-input.js
@@ -1,16 +1,14 @@
-<script>
-    document.addEventListener('DOMContentLoaded', function () {
-        const radioButtons = document.querySelectorAll('input[name="location"]');
-        const selectInput = document.querySelector('.select-input');
+document.addEventListener('DOMContentLoaded', function () {
+    const radioButtons = document.querySelectorAll('input[name="location"]');
+    const selectInput = document.querySelector('.select-input');
 
-        radioButtons.forEach(radio => {
-            radio.addEventListener('change', function () {
-                if (this.value === 'regional') {
-                    selectInput.style.display = 'block';
-                } else {
-                    selectInput.style.display = 'none';
-                }
-            });
+    radioButtons.forEach(radio => {
+        radio.addEventListener('change', function () {
+            if (this.value === 'regional') {
+                selectInput.style.display = 'block';
+            } else {
+                selectInput.style.display = 'none';
+            }
         });
     });
-</script>
+});
